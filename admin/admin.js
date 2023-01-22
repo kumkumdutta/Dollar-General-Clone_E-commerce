30c83e26bdb7e498de34f962ecf00c6701f47914
kumkum19 <kumkumdutta.matelli@gmail.com>
2023-01-22 12:15:29 +0530

    done this

--- admin/admin.js
+++ admin/admin.js
@@ -1,17 +1,19 @@
-// let username = document.getElementById("username");
-// let password = document.getElementById("password");
-// let btn = document.getElementById("login");
-let form = document.getElementById("myform")
-form.addEventListener("submit",function(e){
-  e.preventDefault()
-  console.log(form.username.value)
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+
+// let anchr = document.getElementById("anchr")
+// let form = document.getElementById("myform")
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click",function(){
+  
     
-    // if(form.username.value==="admin" && form.password.value === "admin"){
-    //   console.log("y")
-    //   // anchr.href="../admin/dashboard.html"
-    // }else{
-    //     alert("wrong credential")
-    // }
+    if(username.value==="admin" && password.value === "admin"){
+      console.log("y")
+      anchr.setAttribute("href","./dashboard.html")
+    }else{
+        alert("wrong credential")
+    }
 })
-// ./dashboard.html
+
 
